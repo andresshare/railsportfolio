5fb1d3194012d5e2468c90dccf2db91ec96f3d31
andresshare <andresshare@gmail.com>
2019-08-24 19:40:58 -0500

    Integrated new and create actions for porfolio

--- app/controllers/portfolio_abs_controller.rb
+++ app/controllers/portfolio_abs_controller.rb
@@ -1,39 +1,39 @@
-class PortfolioAbsController < ApplicationController
+  class PortfolioAbsController < ApplicationController
 
   def index
     @portfolio_items = PortfolioAb.all
   end
-  ef new
-  @portfolio_item = Portfolio.new
-end
+  def new
+    @portfolio_item = PortfolioAb.new
+  end
 
-def create
-  @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+  def create
+    @portfolio_item = PortfolioAb.new(params.require(:portfolio).permit(:title, :subtitle, :body))
 
-  respond_to do |format|
-    if @portfolio_item.save
-      format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
-    else
-      format.html { render :new }
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
+      else
+        format.html { render :new }
+      end
     end
   end
-end
 
-def edit
-  @portfolio_item = Portfolio.find(params[:id])
-end
+  def edit
+    @portfolio_item = PortfolioAb.find(params[:id])
+  end
 
-def update
-  @portfolio_item = Portfolio.find(params[:id])
+  def update
+    @portfolio_item = PortfolioAb.find(params[:id])
 
-  respond_to do |format|
-    if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
-      format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
-    else
-      format.html { render :edit }
+    respond_to do |format|
+      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+        format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
+      else
+        format.html { render :edit }
+      end
     end
   end
-end
 
 
 end
