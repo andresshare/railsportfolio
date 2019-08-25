e52604b01476be0fb432bdf732a4fb5011249865
andresshare <andresshare@gmail.com>
2019-08-24 22:19:51 -0500

    config routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  resources :portfolio_abs
-  get 'pages/home'
-  get 'pages/about'
-  get 'pages/contact'
-  resources :blogs
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+  Rails.application.routes.draw do
+    resources :portfolios, except: [:show]
+    get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
+
+    get 'about-me', to: 'pages#about'
+    get 'contact', to: 'pages#contact'
+
+    resources :blogs
+
+    root to: 'pages#home'
+  end
